23ed08252a9ac15c78346f080211f86dc8040419
ahmed hamzaoui <ahmedhamzaoui2003@gmail.com>
2024-11-30 09:06:43 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+    enum enCalculator
+    {
+        eAddition = 1,
+        eSubtraction = 2,
+        eMultiplication = 3,
+        eDivision = 4
+    }
+    internal class Program
+    {
+
+        static double ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            double num = Convert.ToInt32(Console.ReadLine());
+
+            return num;
+        }
+
+        static enCalculator ReadOperationToCalculate(string message,int from , int to)
+        {
+            int op;
+            do
+            {
+                Console.WriteLine(message);
+                op = Convert.ToInt32(Console.ReadLine());
+
+            } while (Enumerable.Range(from, to).Contains(op));
+            return (enCalculator)op;
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
